90d494cf3bda467822034ec87eab6bd4368da5bb
shwang95 <shwang95@users.noreply.github.com>
2017-10-06 22:23:58 -0400

    finish

--- A5/test_examples/collision_tester.py
+++ A5/test_examples/collision_tester.py
@@ -1,48 +1,27 @@
-"""this is the main part of the assignment"""
+# Copyright 2017 Sihan Wang shwang95@bu.edu
 
-# AUTHOR Sihan Wang shwang95@bu.edu
-# AUTHOR ? ??@bu.edu
-# AUTHOR ??? ???@bu.edu
 import unittest
 import subprocess
 
-#please change this to valid author emails
-AUTHORS = ['shwang95@bu.edu', '??@bu.edu', '???@bu.edu']
+AUTHORS = ['shwang95@bu.edu']
 
-PROGRAM_TO_TEST = "collisionc_13"
+PROGRAM_TO_TEST = "collisionc*"
 
 def runprogram(program, args, inputstr):
     coll_run = subprocess.run(
         [program, *args],
         input=inputstr.encode(),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
-    ret_code = coll_run.returncode
-    program_output = coll_run.stdout.decode()
-    program_errors = coll_run.stderr.decode()
-    return (ret_code, program_output, program_errors)
+        stdout=subprocess.PIPE)
+    return coll_run.stdout.decode()
 
 
 class CollisionTestCase(unittest.TestCase):
     "empty class - write this"
     def test_collision(self):
-        strin = "BAL001 0 0 1 0\nBAL002 5 5 0 -1"
-        correct_out = "2\nBAL001 2 0 1 0\nBAL002 5 3 0 -1\n7\nBAL001 5 -2 0 -1\nBAL002 7 0 1 0\n"
-        (rc,out,errs) = runprogram(PROGRAM_TO_TEST,["2", "7"],strin)
+        strin = "BAL001 0 0 1 0\nBAL002 20 0 -1 0"
+        correct_out = "2\nBAL001 2 0 1 0\nBAL002 18 0 -1 0\n7\nBAL001 3 0 -1 0\nBAL002 17 0 1 0\n"
+        out = runprogram(PROGRAM_TO_TEST,["2", "7"],strin)
         self.assertEqual(out,correct_out)
 
-def main():
-    unittest.main()
-
-    print('Run {} tests'.format(results.testsRun))
-    print('you passed {} tests'.format(tests_passed))
-    for test,output in results.failures:
-        print(">>",test)
-        print(">>",output)
-    for test,output in results.errors:
-        print(">>",test)
-        print(">>",output)
-
 if __name__ == '__main__':
-    main()
-
+    unittest.main()
