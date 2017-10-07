2435d8ac650c6e2c8e4af434c1faca1b57a5c31e
shwang95 <shwang95@users.noreply.github.com>
2017-10-06 22:30:13 -0400

    END

--- A5/collision_tester.py
+++ A5/collision_tester.py
@@ -1,50 +1,30 @@
-"""this is the main part of the assignment"""
+# Copyright 2017 Sihan Wang shwang95@bu.edu
 
-# AUTHOR Sihan Wang shwang95@bu.edu
-# AUTHOR ? ??@bu.edu
-# AUTHOR ??? ???@bu.edu
 import unittest
 import subprocess
 
-#please change this to valid author emails
-AUTHORS = ['shwang95@bu.edu', '??@bu.edu', '???@bu.edu']
+AUTHORS = ['shwang95@bu.edu']
 
 PROGRAM_TO_TEST = "collisionc_0"
+
 
 def runprogram(program, args, inputstr):
     coll_run = subprocess.run(
         [program, *args],
         input=inputstr.encode(),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
-    ret_code = coll_run.returncode
-    program_output = coll_run.stdout.decode()
-    program_errors = coll_run.stderr.decode()
-    return (ret_code, program_output, program_errors)
+        stdout=subprocess.PIPE)
+    return coll_run.stdout.decode()
 
 
 class CollisionTestCase(unittest.TestCase):
     "empty class - write this"
-    def test_collision(self):
-        strin = "BAL001 0 0 1 0\nBAL002 5 5 0 -1"
-        correct_out = "2\nBAL001 2 0 1 0\nBAL002 5 3 0 -1\n7\nBAL001 5 -2 0 -1\nBAL002 7 0 1 0"
-        (rc,out,errs) = runprogram(PROGRAM_TO_TEST,["2 7"],strin)
-        # self.assertEqual(rc,2)
-        self.assertEqual(out,correct_out)
-        # self.assertEqual(errs,"")
 
-def main():
-    unittest.main()
-    results = unittest.result.TestResult()
-    print('Run {} tests'.format(results.testsRun))
-    print('you passed {} tests'.format(tests_passed))
-    for test,output in results.failures:
-        print(">>",test)
-        print(">>",output)
-    for test,output in results.errors:
-        print(">>",test)
-        print(">>",output)
+    def test_collision(self):
+        strin = "BAL001 0 0 1 0\nBAL002 20 0 -1 0"
+        correct_out = "2\nBAL001 2 0 1 0\nBAL002 18 0 -1 0\n7\nBAL001 3 0 -1 0\nBAL002 17 0 1 0\n"
+        out = runprogram(PROGRAM_TO_TEST, ["2", "7"], strin)
+        self.assertEqual(out, correct_out)
+
 
 if __name__ == '__main__':
-    main()
-
+    unittest.main()
--- A5/test_examples/collision_tester.py
+++ A5/test_examples/collision_tester.py
@@ -5,7 +5,8 @@ import subprocess
 
 AUTHORS = ['shwang95@bu.edu']
 
-PROGRAM_TO_TEST = "collisionc*"
+PROGRAM_TO_TEST = "collisionc_0"
+
 
 def runprogram(program, args, inputstr):
     coll_run = subprocess.run(
@@ -17,11 +18,13 @@ def runprogram(program, args, inputstr):
 
 class CollisionTestCase(unittest.TestCase):
     "empty class - write this"
+
     def test_collision(self):
         strin = "BAL001 0 0 1 0\nBAL002 20 0 -1 0"
         correct_out = "2\nBAL001 2 0 1 0\nBAL002 18 0 -1 0\n7\nBAL001 3 0 -1 0\nBAL002 17 0 1 0\n"
-        out = runprogram(PROGRAM_TO_TEST,["2", "7"],strin)
-        self.assertEqual(out,correct_out)
+        out = runprogram(PROGRAM_TO_TEST, ["2", "7"], strin)
+        self.assertEqual(out, correct_out)
+
 
 if __name__ == '__main__':
     unittest.main()
